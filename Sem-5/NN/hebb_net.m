6c728831109365d24825b998ad321d2d5d4f3d27
DEEPANSHU <139230988+I-Deepanshu@users.noreply.github.com>
2024-12-11 22:20:50 +0530

    Update hebb_net.m

--- Sem-5/NN/hebb_net.m
+++ Sem-5/NN/hebb_net.m
@@ -1,28 +1,24 @@
-% Define input patterns (bipolar) and target values
-inputs = [1 1; 1 -1; -1 1; -1 -1]; % 2D bipolar inputs
-targets = [1; -1; -1; -1]; % Target values for each input pattern
+% Hebb Net to classify two dimensional input patterns
+clear;
+clc;
 
-% Initialize weights and bias
-weights = [0; 0]; % 2D weight vector
-bias = 0;
+% Input Patterns
+E = [1 1 1 1 1 -1 -1 -1 -1 -1 1 1 1 1 1 -1 -1 -1 -1 -1];
+F = [1 1 -1 -1 -1 1 1 -1 -1 -1 1 1 -1 -1 -1 1 1 -1 -1 -1];
 
-% Hebbian learning rule
-for i = 1:size(inputs, 1)
-    weights = weights + inputs(i, :)' * targets(i);
-    bias = bias + targets(i);
+x(1:20) = E;
+x(21:40) = F;
+w(1:20) = 0;
+t = [-1 1];
+b = 0;
+
+for i = 1:2
+    w = w + x(1,1:20) * t(1);
+    b = b + t(1);
 end
 
-% Display results
-disp('Weights after Hebbian learning:');
-disp(weights);
-disp('Bias after Hebbian learning:');
-disp(bias);
+disp('Weight matrix');
+disp(w);
+disp('Bias');
+disp(b);
 
-% Testing the Hebb Net
-disp('Testing Hebb Net:');
-
-for i = 1:size(inputs, 1)
-    net_input = dot(weights, inputs(i, :)') + bias;
-    output = sign(net_input); % Bipolar output (-1, 1)
-    fprintf('Input: [%d %d] -> Output: %d (Target: %d)\n', inputs(i, 1), inputs(i, 2), output, targets(i));
-end
